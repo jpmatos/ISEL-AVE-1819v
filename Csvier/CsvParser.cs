9f46d50e71287d25e4d859f4d1d0d8747771970f
João Matos <joao.matos.jm@gmail.com>
2019-03-16 13:03:07 +0000

    Part1 done

---
+++ Csvier/CsvParser.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Csvier
+{
+    public class CsvParser
+    {
+        private struct Pair
+        {
+            public readonly string arg;
+            public readonly int col;
+
+            public Pair(string arg, int col)
+            {
+                this.arg = arg;
+                this.col = col;
+            }
+        }
+
+        private readonly Type klass;
+        private readonly char separator;
+        private string[] arr;
+        private readonly List<Pair> ctorParams;
+        private static readonly Dictionary<Type, MethodBase> parsers = new Dictionary<Type, MethodBase>();
+
+        public CsvParser(Type klass, char separator = ',')
+        {
+            this.klass = klass;
+            this.separator = separator;
+            this.ctorParams = new List<Pair>();
+        }
+
+        public CsvParser CtorArg(string arg, int col)
+        {
+            ctorParams.Add(new Pair(arg, col));
+            return this;
+        }
+
+        public CsvParser PropArg(string arg, int col)
+        {
+            return this;
+        }
+
+        public CsvParser FieldArg(string arg, int col)
+        {
+            return this;
+        }
+
+        public CsvParser Load(String src)
+        {
+            arr = src.Split(new[] {"\r\n", "\r", "\n"}, StringSplitOptions.None);
+            return this;
+        }
+
+        public CsvParser Remove(int count)
+        {
+            return this;
+        }
+
+        public CsvParser RemoveEmpties()
+        {
+            return this;
+        }
+
+        public CsvParser RemoveWith(string word)
+        {
+            return this;
+        }
+
+        public CsvParser RemoveEvenIndexes()
+        {
+            return this;
+        }
+
+        public CsvParser RemoveOddIndexes()
+        {
+            return this;
+        }
+
+        public IEnumerable<object> Parse()
+        {
+            ConstructorInfo con = FindConstructor(); // helper method
+            IEnumerable<object> res = ParseArray(con); // helper method
+            return res;
+        }
+
+        private ConstructorInfo FindConstructor()
+        {
+            ConstructorInfo[] cons = klass.GetConstructors();
+            foreach (ConstructorInfo con in cons)
+            {
+                ParameterInfo[] parameters = con.GetParameters();
+                if (parameters.Length != ctorParams.Count)
+                    continue;
+                using (List<Pair>.Enumerator enumerator = ctorParams.GetEnumerator())
+                {
+                    enumerator.MoveNext();
+                    foreach (ParameterInfo parameter in parameters)
+                    {
+                        if (parameter.Name != enumerator.Current.arg)
+                            break;
+                        if (!enumerator.MoveNext())
+                            return con;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private IEnumerable<object> ParseArray(ConstructorInfo con)
+        {
+            ParameterInfo[] parameters = con.GetParameters();
+            object[] res = new object[arr.Length];
+            object[] args = new object[ctorParams.Count];
+            for (int i = 0; i < res.Length; i++)
+            {
+                string[] values = arr[i].Split(separator);
+                for (int j = 0; j < args.Length; j++)
+                {
+                    Type paramType = parameters[j].ParameterType;
+                    if (!parsers.TryGetValue(paramType, out var parser))
+                    {
+                        parser = paramType.GetMethod("Parse", new Type[] {typeof(string)});
+                        parsers.Add(paramType, parser);
+                    }
+
+                    if (parser == null) throw new MissingMethodException();
+                    int index = ctorParams[j].col;
+                    string value = values[index];
+                    args[j] = parser.Invoke(null, new object[] {value});
+                }
+
+                res[i] = con.Invoke(args);
+            }
+
+            return res;
+        }
+    }
+}
---
+++ WingTest/ClimaTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using NUnit.Framework;
+using System.Net;
+using Clima;
+using System.Collections.Generic;
+
+namespace Csvier.Test
+{
+    [TestFixture]
+    public class ClimaTest
+    {
+        [Test]
+        public void TestLoadSearchOporto()
+        {
+            using (WeatherWebApi api = new WeatherWebApi())
+            {
+                LocationInfo[] locals = api.Search("oporto");
+                Assert.AreEqual(6, locals.Length);
+                Assert.AreEqual("Cuba", locals[5].Country);
+            }
+        }
+
+        [Test]
+        public void TestLoadPastWeatherOnJanuaryAndCheckMaximumTempC()
+        {
+            using (WeatherWebApi api = new WeatherWebApi())
+            {
+                IEnumerable<WeatherInfo> infos = api.PastWeather(37.017, -7.933, DateTime.Parse("2019-01-01"),
+                    DateTime.Parse("2019-01-30"));
+                int max = int.MinValue;
+                foreach (WeatherInfo wi in infos)
+                {
+                    if (wi.TempC > max) max = wi.TempC;
+                }
+
+                Assert.AreEqual(19, max);
+                // Console.WriteLine(String.Join("\n", infos));
+            }
+        }
+
+        [Test]
+        public void CleanCvsExampleTEst()
+        {
+            string sampleWeatherInLisbonFiltered =
+                "2019-01-01,24,17,63,6,10,74,ENE,116,http://cdn.worldweatheronline.net/images/wsymbols01_png_64/wsymbol_0002_sunny_intervals.png,Partly cloudy,0.0,59,10,1031,43,14,57,6,43,13,56,11,17,13,56\n" +
+                "2019-01-02,24,18,64,6,9,179,S,116,http://cdn.worldweatheronline.net/images/wsymbols01_png_64/wsymbol_0002_sunny_intervals.png,Partly cloudy,0.0,57,10,1030,15,14,57,6,42,13,56,11,17,13,56\n" +
+                "2019-01-03,24,16,60,7,11,89,E,113,http://cdn.worldweatheronline.net/images/wsymbols01_png_64/wsymbol_0001_sunny.png,Sunny,0.0,67,10,1026,3,13,55,7,45,12,54,11,18,12,54\n" +
+                "2019-01-04,24,16,60,9,15,78,ENE,116,http://cdn.worldweatheronline.net/images/wsymbols01_png_64/wsymbol_0002_sunny_intervals.png,Partly cloudy,0.1,73,10,1028,27,14,57,9,48,13,55,14,23,13,55";
+
+            CsvParser pastWeather = new CsvParser(typeof(WeatherInfo))
+                .CtorArg("date", 0)
+                .CtorArg("tempC", 2);
+            IEnumerable<object> items = pastWeather
+                .Load(sampleWeatherInLisbonFiltered)
+                .Parse();
+
+            WeatherInfo[] expected = new WeatherInfo[]
+            {
+                new WeatherInfo(new DateTime(2019, 1, 1), 17),
+                new WeatherInfo(new DateTime(2019, 1, 2), 18),
+                new WeatherInfo(new DateTime(2019, 1, 3), 16),
+                new WeatherInfo(new DateTime(2019, 1, 4), 16)
+            };
+
+            int i = 0;
+            foreach (WeatherInfo item in items)
+            {
+                Assert.AreEqual(expected[i].Date, item.Date);
+                Assert.AreEqual(expected[i].TempC, item.TempC);
+                i++;
+            }
+        }
+    }
+}
